88305212dd7e103a8c74c6c6f3657f48e283a59d
Mark Varadi <mark.varadi03@gmail.com>
2021-08-18 18:23:01 +0300

    code cleanup and fixes

--- client-service/src/main/kotlin/ro/asis/client/service/repository/ClientDao.kt
+++ client-service/src/main/kotlin/ro/asis/client/service/repository/ClientDao.kt
@@ -6,7 +6,7 @@ import org.springframework.data.mongodb.core.query.Query
 import org.springframework.stereotype.Repository
 import ro.asis.client.service.model.entity.ClientEntity
 import ro.asis.commons.filters.ClientFilters
-import java.util.*
+import java.util.Optional.ofNullable
 
 @Repository
 class ClientDao(private val mongo: MongoTemplate) {
@@ -22,14 +22,16 @@ class ClientDao(private val mongo: MongoTemplate) {
     private fun buildCriteria(filters: ClientFilters): List<Criteria> {
         val criteria = mutableListOf<Criteria>()
 
-        Optional.ofNullable(filters.firstName)
+        ofNullable(filters.firstName)
             .ifPresent { criteria.add(Criteria.where("firstName").regex(".*$it.*", "i")) }
-        Optional.ofNullable(filters.lastName)
+        ofNullable(filters.lastName)
             .ifPresent { criteria.add(Criteria.where("lastName").regex(".*$it.*", "i")) }
-        Optional.ofNullable(filters.city)
+        ofNullable(filters.city)
             .ifPresent { criteria.add(Criteria.where("address.city").regex(".*$it.*", "i")) }
-        Optional.ofNullable(filters.streetName)
+        ofNullable(filters.streetName)
             .ifPresent { criteria.add(Criteria.where("address.streetName").regex(".*$it.*", "i")) }
+        ofNullable(filters.accountId)
+            .ifPresent { criteria.add(Criteria.where("accountId").`is`(it)) }
 
         return criteria
     }
